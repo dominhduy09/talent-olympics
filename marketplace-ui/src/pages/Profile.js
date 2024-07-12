dc743eeb455600057b8ecf45f456913059c35f94
Do Minh Duy <dominhduy09@gmail.com>
2024-07-12 22:00:05 +0700

    add profile css

---
+++ marketplace-ui/src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div className="profile-container">
+            <h1>User Profile</h1>
+            <p>This is where you can display user-specific information.</p>
+        </div>
+    );
+};
+
+export default Profile;
+
+
+
